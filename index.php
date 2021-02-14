570e75e894de0e7b86148d4df0f03db1aeefd9f1
AymenB98 <60265156+AymenB98@users.noreply.github.com>
2021-02-14 13:13:15 +0000

    NGINX server setup on pi with PHP enabled.
    This server hosts a basic web page which has the product logo and current date. This will be developed alongside the hardware drivers to display how many birds have visited.
    This also has a link to the GitHub repo for this project.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to the Bird Cafe!</title>
+<style>
+    body {
+        width: 35em;
+        margin: 0 auto;
+        font-family: Tahoma, Verdana, Arial, sans-serif;
+    }
+</style>
+</head>
+<body>
+<center><h1>Welcome to the Bird Cafe</h1>
+<img src="bird_logo_pink.jpg">
+<h1 style="color:#c966a4;font-size:80px;"><b>0</b></h1>
+
+<?php echo "Birds have visted your cafe today: " . date("d/m/y") . "</br>"; ?>
+
+<p style="color:#c966a4;font-size:70px;" You can view the code for this project at the following link:</p>
+<a style="color:#c966a4;font-size:50px;" href="https://github.com/AymenB98/Bird-Cafe">Bird Cafe GitHub repo. </a>
+</centre>
+</body>
+</html>
