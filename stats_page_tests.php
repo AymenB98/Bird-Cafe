77ce382998d63d287e51e492c5a71b2066644ad3
AymenB98 <60265156+AymenB98@users.noreply.github.com>
2021-03-04 22:40:14 +0000

    Code coverage enabled and tested.
    Comments added to test the code coverage. The analysis showed 100% code coverage. A typo in this file that was not being picked up during compilation was also fixed.

--- stats_page_tests.php
+++ stats_page_tests.php
@@ -1,8 +1,17 @@
-<?php
+<?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 // Test to make sure the number of birds is positive
+
+/**
+ * @covers \VisitStats
+ * @covers \CsvCreate
+ */
 class statsPageTest extends TestCase
 {
+	
+	/**
+	 * @covers \VisitStats::birdCount
+	 */
 	public function testCountIsPositive()
 	{
 		$crow = new VisitStats();
@@ -10,20 +19,27 @@ class statsPageTest extends TestCase
 		$this->assertGreaterThanOrEqual(0, $count);
 	}
 	
+	/**
+	 * @coversNothing
+	 */
 	public function testCsvCreate()
 	{
+		// Delete file before creating it again.
+		// This means that the file does not exist
+		// before the test is created.
 		$csvTest = new CsvCreate(7, 2);
-		$this->assertFileExists("visits.csv");
+		$this->assertFileExists("visitNumber.csv");
 	}
 	
+	/**
+	 * @covers \CsvCreate::pythonScript
+	 */
 	public function testPythonExec()
 	{
-		$pythonTest = new csVCreate(3, 1);
+		$pythonTest = new CsvCreate(7, 2);
 		$execValue = $pythonTest->pythonScript();
 		$this->assertEquals($execValue, "Python script running");
-		
 	}
-	
-}
 
+}
 ?>
