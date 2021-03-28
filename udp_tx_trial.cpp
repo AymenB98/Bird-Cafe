da2413c7a1d4a8b9743c72146b5a6344d32e8e1f
AymenB98 <60265156+AymenB98@users.noreply.github.com>
2021-03-28 17:22:54 +0100

    Documentation added to code.

--- udp_tx_trial.cpp
+++ udp_tx_trial.cpp
@@ -1,3 +1,20 @@
+// Author: Aymen Benylles
+// This code was adapted from the file "udp_transmit.cpp"
+// from the GitHub repo: https://github.com/berndporr/rpi_AD7705_daq
+
+/*
+ * AD7705 test/demo program for the Raspberry PI
+ *
+ * Copyright (c) 2007  MontaVista Software, Inc.
+ * Copyright (c) 2007  Anton Vorontsov <avorontsov@ru.mvista.com>
+ * Copyright (c) 2013-2020  Bernd Porr <mail@berndporr.me.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ * Cross-compile with cross-gcc -I/path/to/cross-kernel/include
+ */
 #include <stdint.h>
 #include <unistd.h>
 #include <stdio.h>
