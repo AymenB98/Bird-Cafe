c70b78ff11736596c3b0240fd08c52897820cc8f
JoMiCos <joecostello1998@gmail.com>
2021-04-02 19:10:09 +0100

    Added functionality for Ultrasonic Sesnor. Integrated Camera with Ultrasonic Sesnor.

---
+++ drivers/USsensor.cpp
@@ -0,0 +1,83 @@
+//!**************************************************************
+//! \file
+//! \brief Defines Ultrasonic subclass
+//!**************************************************************
+
+#include "USsensor.h"
+
+float Ultrasonic::distanceCalcUS(float pulseTime)
+{
+    float pulseDist = (pulseTime*speed_of_sound)/2;
+    return pulseDist;
+}
+
+float Ultrasonic::measureDistance()
+{
+    digitalWrite(trig, LOW); //trigger pin low for 2us, clears pin
+    delayMicroseconds(2);
+    digitalWrite(trig, HIGH); //trigger pin high for 10us to send pulse
+    delayMicroseconds(10);
+    digitalWrite(trig, LOW); //trigger pin low to end 10us pulse
+    
+    while (digitalRead(echo) == LOW)
+    {
+        // wait until echo pin HIGH (receives echo)
+    }
+    
+    std::chrono::steady_clock::time_point timerStart = std::chrono::steady_clock::now(); //start timer
+    while (digitalRead(echo) == HIGH)
+    {
+        //wait while echo pin High
+    }
+    std::chrono::steady_clock::time_point timerStop = std::chrono::steady_clock::now(); //stop timer
+
+    float pulseDur = std::chrono::duration<float>(timerStop-timerStart).count(); //force timer to float
+    float distance = distanceCalcUS(pulseDur);
+    std::cout << distance << std::endl;
+
+    return distance;
+}
+
+void Ultrasonic::setTrigger(int Trig)
+{
+    trig = Trig;
+}
+
+void Ultrasonic::setEcho(int Echo)
+{
+    echo = Echo;
+}
+
+void Ultrasonic::start()
+{
+    if (USThread)
+    {
+        throw "ERROR: Thread already running.";
+    }
+        USThread = new std::thread(run, this);
+}
+
+void Ultrasonic::stop()
+{
+    running = 0;
+    if (USThread)
+    {
+        USThread->join();
+        delete USThread;
+        USThread = NULL;
+    }
+}
+
+void Ultrasonic::run(Ultrasonic* ultrasonic)
+{
+    ultrasonic->running =1;
+    while (ultrasonic->running)
+    {
+        if(ultrasonic->measureDistance() < 1.0)
+        {
+            ultrasonic->takePhoto();
+            std::this_thread::sleep_for(std::chrono::seconds(30));
+        }
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
---
+++ drivers/USsensor.h
@@ -0,0 +1,62 @@
+//!**************************************************************
+//! Joe Costello 02/04/2021
+//! \file
+//! \brief Header file for Ultrasonic class
+//!**************************************************************
+
+#ifndef _BCULTRASOUND_H
+#define _BCULTRASOUND_H
+
+
+#include <wiringPi.h>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <ostream>
+#include "BirdCam.h"
+
+
+//! Class used for interfacing ultrasonic sensor
+class Ultrasonic : public BirdCam
+{
+    private:
+    //initialisation
+    const int speed_of_sound = 343;
+    int trig;
+    int echo;
+    int running = 0; 
+    std::thread* USThread = NULL;
+    
+    //! \brief Converts the time taken for a US pulse to return into a distance.
+    float distanceCalcUS(float pulseTime);
+    
+    public:
+
+    //! \brief Constructor for Ultrasonic class. Ultrasnoic(int TriggerPin, int EchoPin)
+    Ultrasonic(int Trig, int Echo)
+    {
+        echo = Echo;
+        trig = Trig;
+    }
+
+    //setters
+    //! \brief Used to set trigger pin.
+    void setTrigger(int Trig);
+
+    //! \brief Used to set echo pin.
+    void setEcho(int Echo);
+
+    //! \brief Function which triggers ultrasound pulse, reads echo pin, finds difference and returns distance in metres.
+    float measureDistance();
+
+    //! \brief Runs ultrasonic sensor routine
+    static void run(Ultrasonic* ultrasonic);
+    
+    //! \brief Starts thread for ultrasonic sensor, checks its not already running
+    void start();
+    
+    //! \brief Stops + tidies ultrasonic sensor thread.
+    void stop();
+
+};
+#endif
